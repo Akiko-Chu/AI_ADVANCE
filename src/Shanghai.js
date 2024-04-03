0d327932b989d834412402fc97a8b42d4e97cc1f
Akiko <akiko990901@gmail.com>
2024-04-03 16:18:40 +1100

    fix image

--- src/Shanghai.js
+++ src/Shanghai.js
@@ -2,27 +2,29 @@ import React from 'react';
 import './Slide.css';
 import { Carousel, Image } from 'react-bootstrap';
 
+import geolife1 from './Image/geolife1.png';
+
 function Shanghai() {
   return (
     <div>
       <h2>Shanghai Dataset</h2>
       <Carousel>
         <Carousel.Item>
-            <Image className="d-block w-100" src="Image/geolife1.png" alt="First slide" />
+            <Image className="d-block w-100" src={geolife1} alt="First slide" />
             <Carousel.Caption>
             <h3>First slide label</h3>
             <p>Data exploration 1 - ...</p>
             </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-            <Image className="d-block w-100" src="Image/geolife1.png" alt="Second slide" />
+            <Image className="d-block w-100" src={geolife1} alt="Second slide" />
             <Carousel.Caption>
             <h3>Second slide label</h3>
             <p>Data exploration 2 - ...</p>
             </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-            <Image className="d-block w-100" src="Image/geolife1.png" alt="Third slide" />
+            <Image className="d-block w-100" src={geolife1} alt="Third slide" />
             <Carousel.Caption>
             <h3>Third slide label</h3>
             <p>Data exploration 3 - ...</p>
